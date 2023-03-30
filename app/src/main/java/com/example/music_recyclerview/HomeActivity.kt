380e3f313adc1e82803c9caa6077cc24914e16cd
Sajadio <sjd04969@gmail.com>
2023-03-31 00:36:50 +0300

    initial commit

---
+++ app/src/main/java/com/example/music_recyclerview/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.example.music_recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.music_recyclerview.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
