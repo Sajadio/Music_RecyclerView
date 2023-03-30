0bc3724a91df355ee0fca8fed879de85bff4ca37
Sajadio <sjd04969@gmail.com>
2023-03-31 01:57:20 +0300

    create an adapter for music recyclerview

--- app/src/main/java/com/example/music_recyclerview/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/example/music_recyclerview/ui/activity/HomeActivity.kt
@@ -1,16 +1,24 @@
 package com.example.music_recyclerview.ui.activity
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.music_recyclerview.R
 import com.example.music_recyclerview.databinding.ActivityHomeBinding
+import com.example.music_recyclerview.ui.fragment.HomeFragment
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
 
+    @SuppressLint("CommitTransaction")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container_music, HomeFragment())
+            commit()
+        }
     }
 }
--- app/src/main/java/com/example/music_recyclerview/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/music_recyclerview/ui/fragment/HomeFragment.kt
@@ -1,4 +1,25 @@
 package com.example.music_recyclerview.ui.fragment
 
-class HomeFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.music_recyclerview.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
 }
