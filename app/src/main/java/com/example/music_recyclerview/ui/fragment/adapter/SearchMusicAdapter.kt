f9e84e1725515b7d6648c84b5c21c0fe236651c2
Sajadio <sjd04969@gmail.com>
2023-03-31 02:34:53 +0300

    create search news adapter

---
+++ app/src/main/java/com/example/music_recyclerview/ui/fragment/adapter/SearchMusicAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.music_recyclerview.ui.fragment.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.music_recyclerview.data.Music
+import com.example.music_recyclerview.databinding.ItemCardNewMusicBinding
+import com.example.music_recyclerview.utils.loadImage
+
+class SearchMusicAdapter(
+    private val items: List<Music>
+) :
+    RecyclerView.Adapter<SearchMusicAdapter.SearchMusicViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchMusicViewHolder {
+        val inflate = LayoutInflater.from(parent.context)
+        val view = ItemCardNewMusicBinding.inflate(inflate, parent, false)
+        return SearchMusicViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SearchMusicViewHolder, position: Int) {
+        val music = items[position]
+        with(holder.binding) {
+            imageViewMusic.loadImage(music.image)
+            textViewSongTitle.text = music.title
+            textViewSongWriter.text = music.songWriters
+        }
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    inner class SearchMusicViewHolder(val binding: ItemCardNewMusicBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+    }
+}
